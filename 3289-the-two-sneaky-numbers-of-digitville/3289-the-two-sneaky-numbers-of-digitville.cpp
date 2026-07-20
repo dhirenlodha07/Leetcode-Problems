92467ba6b3a542e2faf41ee6da85d86cd512e0eb
dhirenlodha07 <dhirenlodha07@gmail.com>
2026-07-20 20:20:14 +0530

    Time: 0 ms (100%), Space: 25.3 MB (92.66%) - LeetHub

---
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+    vector<int>result;
+    sort(nums.begin(),nums.end());
+
+    for(int i=1;i<nums.size();i++) {
+        if(nums[i]==nums[i-1]) {
+            result.push_back(nums[i]);
+            i++;
+        }
+       
+    }
+
+    return result;
+        
+    }
+};
