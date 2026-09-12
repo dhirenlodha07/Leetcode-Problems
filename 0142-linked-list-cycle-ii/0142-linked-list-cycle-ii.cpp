ef651e48cd40b315527e80c9fab0a2c44a853b68
Dhiren Lodha <dhirenlodha07@gmail.com>
2026-09-12 22:04:17 +0530

    Time: 10 ms (43.73%), Space: 11.2 MB (85.52%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,15 +9,21 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-    map<ListNode*,int>mpp;
-    ListNode* temp = head;
-    while(temp!=NULL){
-        if(mpp.count(temp)==1) {
-            return temp;
+   //tortoise and hare alg : The optimal for detection of cycle in a ll
+    ListNode* slow = head;
+    ListNode* fast =  head;
+    while(fast!=NULL && fast->next!=NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow==fast) { //cycle detected
+            slow = head; //reset slow and thenn move both by one step for second collision pt
+            while(slow!=fast) {
+                slow = slow->next;
+                fast = fast->next;
+            }
+            return fast; //or slow
         }
-        mpp[temp] = 1;
-        temp = temp->next;
-     } 
-     return NULL;     
+    }
+    return NULL;
     }
 };
